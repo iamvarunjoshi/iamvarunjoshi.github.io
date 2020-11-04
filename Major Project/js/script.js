5d6180d66ebb91eaf763464f1896b012b7ce9716
Varun Joshi <54651150+iamvarunjoshi@users.noreply.github.com>
2020-11-04 21:51:48 +0530

    Update script.js

--- Major Project/js/script.js
+++ Major Project/js/script.js
@@ -44,26 +44,115 @@ var chart = new CanvasJS.Chart("chartContainer", {
 	animationEnabled: true,
 	theme: "light2",
 	title:{
-		text: "Your Charging History"
+		text: "Power Consumption"
 	},
 	data: [{        
 		type: "line",
       	indexLabelFontSize: 16,
+		name: "Power Consumption",
+		showInLegend: true,
+		yValueFormatString: "#W",
 		dataPoints: [
-			{ y: 45 },
-			{ y: 41},
-			{ y: 52, indexLabel: "\u2191 highest",markerColor: "red", markerType: "triangle" },
-			{ y: 46 },
-			{ y: 45 },
-			{ y: 50 },
-			{ y: 48 },
-			{ y: 48 },
-			{ y: 41 , indexLabel: "\u2193 lowest",markerColor: "DarkSlateGrey", markerType: "cross" },
-			{ y: 50 },
-			{ y: 48 },
-			{ y: 51 }
+			{ x: new Date(2020, 0),y: 450 },
+			{ x: new Date(2020, 1),y: 410},
+			{ x: new Date(2020, 2),y: 520, indexLabel: "\u2191 highest",markerColor: "red", markerType: "triangle" },
+			{ x: new Date(2020, 3),y: 460 },
+			{ x: new Date(2020, 4),y: 450 },
+			{ x: new Date(2020, 5),y: 500 },
+			{ x: new Date(2020, 6),y: 480 },
+			{ x: new Date(2020, 7),y: 480 },
+			{ x: new Date(2020, 8),y: 410 , indexLabel: "\u2193 lowest",markerColor: "DarkSlateGrey", markerType: "cross" },
+			{ x: new Date(2020, 9),y: 500 },
+			{ x: new Date(2020, 10),y: 408 },
+			{ x: new Date(2020, 11),y: 510 }
 		]
 	}]
 });
-chart.render();
+
+/*Old Chart2 showing monthly sales.*/    
+var chart1 = new CanvasJS.Chart("chartContainer2", {
+	animationEnabled: true,
+	theme: "light2",
+	title: {
+		text: "Past Savings"
+	},
+	axisX: {
+		valueFormatString: "MMM"
+	},
+	axisY: {
+		prefix: "$",
+		labelFormatter: addSymbols
+	},
+	toolTip: {
+		shared: true
+	},
+	legend: {
+		cursor: "pointer",
+		itemclick: toggleDataSeries
+	},
+	data: [
+	{
+		type: "column",
+		name: "Actual Savings",
+		showInLegend: true,
+		xValueFormatString: "MMMM YYYY",
+		yValueFormatString: "$#,##0",
+		dataPoints: [
+			{ x: new Date(2020, 0), y: 20000 },
+			{ x: new Date(2020, 1), y: 30000 },
+			{ x: new Date(2020, 2), y: 25000 },
+			{ x: new Date(2020, 3), y: 70000, indexLabel: "High Renewals" },
+			{ x: new Date(2020, 4), y: 50000 },
+			{ x: new Date(2020, 5), y: 35000 },
+			{ x: new Date(2020, 6), y: 30000 },
+			{ x: new Date(2020, 7), y: 43000 },
+			{ x: new Date(2020, 8), y: 35000 },
+			{ x: new Date(2020, 9), y:  30000},
+			{ x: new Date(2020, 10), y: 40000 },
+			{ x: new Date(2020, 11), y: 50000 }
+		]
+	}, 
+	{
+		type: "line",
+		name: "Expected Savings",
+		showInLegend: true,
+		yValueFormatString: "$#,##0",
+		dataPoints: [
+			{ x: new Date(2020, 0), y: 40000 },
+			{ x: new Date(2020, 1), y: 42000 },
+			{ x: new Date(2020, 2), y: 45000 },
+			{ x: new Date(2020, 3), y: 45000 },
+			{ x: new Date(2020, 4), y: 47000 },
+			{ x: new Date(2020, 5), y: 43000 },
+			{ x: new Date(2020, 6), y: 42000 },
+			{ x: new Date(2020, 7), y: 43000 },
+			{ x: new Date(2020, 8), y: 41000 },
+			{ x: new Date(2020, 9), y: 45000 },
+			{ x: new Date(2020, 10), y: 42000 },
+			{ x: new Date(2020, 11), y: 50000 }
+		]
+	}]
+});
+chart.render();    
+chart1.render();
+
+function addSymbols(e) {
+	var suffixes = ["", "K", "M", "B"];
+	var order = Math.max(Math.floor(Math.log(e.value) / Math.log(1000)), 0);
+
+	if(order > suffixes.length - 1)                	
+		order = suffixes.length - 1;
+
+	var suffix = suffixes[order];      
+	return CanvasJS.formatNumber(e.value / Math.pow(1000, order)) + suffix;
+}
+
+function toggleDataSeries(e) {
+	if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+		e.dataSeries.visible = false;
+	} else {
+		e.dataSeries.visible = true;
+	}
+	e.chart1.render();
+}    
 }
